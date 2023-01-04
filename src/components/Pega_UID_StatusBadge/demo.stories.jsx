a1420be0ad2a2fcdfe5634d87137d8134ecc9a24
Francine Ong <francine.ong@pega.com>
2023-01-04 14:05:58 +0100

    feat: status badge component and github homage definition

---
+++ src/components/Pega_UID_StatusBadge/demo.stories.jsx
@@ -0,0 +1,74 @@
+import { withKnobs } from '@storybook/addon-knobs';
+
+import PegaUidStatusBadge from './index';
+
+export default {
+  title: 'PegaUidStatusBadge',
+  decorators: [withKnobs],
+  component: PegaUidStatusBadge
+};
+
+export const successStatusBadge = () => {
+  const props = {
+    labelText: 'Success',
+    variant: 'success'
+  };
+
+  return (
+    <>
+      <PegaUidStatusBadge {...props} />
+    </>
+  );
+};
+
+export const urgentStatusBadge = () => {
+  const props = {
+    labelText: 'Urgent',
+    variant: 'urgent'
+  };
+
+  return (
+    <>
+      <PegaUidStatusBadge {...props} />
+    </>
+  );
+};
+
+export const warnStatusBadge = () => {
+  const props = {
+    labelText: 'Warn',
+    variant: 'warn'
+  };
+
+  return (
+    <>
+      <PegaUidStatusBadge {...props} />
+    </>
+  );
+};
+
+export const pendingStatusBadge = () => {
+  const props = {
+    labelText: 'pending',
+    variant: 'pending'
+  };
+
+  return (
+    <>
+      <PegaUidStatusBadge {...props} />
+    </>
+  );
+};
+
+export const infoStatusBadge = () => {
+  const props = {
+    labelText: 'info',
+    variant: 'info'
+  };
+
+  return (
+    <>
+      <PegaUidStatusBadge {...props} />
+    </>
+  );
+};
