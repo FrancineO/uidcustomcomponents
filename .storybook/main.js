32a90d3be1b6609e522f1d79004bce8857419352
Francine Ong <francine.ong@pega.com>
2023-01-26 13:53:23 +0100

    feat: Member card update with storybook controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: 'webpack5'
   }
